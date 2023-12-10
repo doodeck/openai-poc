976a50d4f983daa7c055b59c47612e3cc931f254
doodeck <8515450+doodeck@users.noreply.github.com>
2023-12-10 12:41:06 +0000

    Migrate to the contemporary API

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,13 @@
-import openai
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
 
-openai.api_key = 'your api key'
+load_dotenv()
+client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="Here is a simple Python script to test the OpenAI API:",
-  temperature=0,
-  max_tokens=1000
-)
+response = client.completions.create(model="text-davinci-003",
+prompt="Here is a simple Python script to test the OpenAI API:",
+temperature=0,
+max_tokens=1000)
 
 print(response.choices[0].text)
